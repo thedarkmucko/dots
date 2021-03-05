7f3290dc285d00c231c87288904ce3dc44c73400
PeterSorger <sorger.cee@gmail.com>
2021-03-05 07:02:09 +0100

    new dir layout

---
+++ helpers/EmailObject.py
@@ -0,0 +1,130 @@
+import datetime
+import helpers as helpme
+import pathlib
+import csv
+
+
+class EmailObject:
+    """Email Object is the core information from the announcement.csv"""
+
+    def __init__(self, **kwargs):
+
+        for key, value in kwargs.items():
+
+            if key == 'system':
+                system_list = value.split(';')
+                self._system = system_list
+
+            if key == 'services':
+                self._services = value
+
+            if key == 'starttime':
+                if not value:
+                    self._start_time = datetime.min
+                else:
+                    self._start_time = helpme.cd_to_datetime(value)
+
+            if key == 'endtime':
+                if not value:
+                    self._end_time = datetime.min
+                else:
+                    self._end_time = helpme.cd_to_datetime(value)
+
+            if key == 'downtime':
+                if value == 'Y':
+                    self._downtime = 'YES'
+                else:
+                    self._downtime = 'NO'
+
+            if key == 'approval':
+                if value == 'Y':
+                    self._approval = 'YES'
+                else:
+                    self._approval = 'NO'
+
+            if key == 'receivers':
+                if not value:
+                    self._receivers = None
+                else:
+                    mylist: list = value.split(';')
+                    self._receivers = mylist
+
+            if key == 'cc':
+                if value:
+                    mylist = value.split(';')
+                    self._cc = mylist
+                else:
+                    self._cc = None
+
+            if key == 'task':
+                self._task = value
+
+            if key == 'contact':
+                self._contact = value
+
+            if key == 'contact_no':
+                self._contact_no = value
+
+    @property
+    def receivers(self) -> list[str]:
+        return self._receivers
+
+    @property
+    def cc(self) -> list[str]:
+        return self._cc
+
+    @property
+    def system(self):
+        return self._system
+
+    @property
+    def services(self) -> str:
+        out = '<h3>Affected Services:</h3>'
+        items = self._services.split(";")
+        for item in items:
+            out += item + "<br>"
+        return out
+
+    def __str__(self):
+        return (f" Affected Systems: {list_to_string(self._system)} - "
+                f"Start Time: {helpme.datetime_to_str(self._start_time)}, "
+                f"Approval required: {str(self._approval)}, "
+                f"Downtime: {self._downtime}")
+
+    @property
+    def start_time(self):
+        return self._start_time
+
+    @property
+    def end_time(self):
+        return self._end_time
+
+
+
+def load_csv(file):
+    """Returns a list of EmailObject Objects"""
+
+    infile = pathlib.Path(file)
+    out = list()
+
+    with open(infile, 'r') as _io:
+        data = csv.DictReader(_io)
+        for row in data:
+            o_email = EmailObject(system=row['system'],
+                                  services=row['services'],
+                                  starttime=row['starttime'],
+                                  endtime=row['endtime'],
+                                  downtime=row['downtime'],
+                                  approval=row['approval'],
+                                  receivers=row['receiver'],
+                                  cc=row['cc'],
+                                  task=row['task'],
+                                  contact=row['contact'],
+                                  contact_no=row['contact_no']
+                                  )
+            out.append(o_email)
+    return out
+
+
+def list_to_string(i_list):
+    return ' '.join([str(elem) for elem in i_list])
--- mailer.py
+++ mailer.py
@@ -1,148 +1,21 @@
 import argparse
-import csv
 import pathlib
 import smtplib
 from datetime import datetime
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-import helpers
-from enum import Enum
 from jinja2 import Environment, FileSystemLoader
+
+from .helpers import EmailObject
 
 PROJECT_ROOT = pathlib.Path(__file__).parent.resolve()
 DATA_ROOT = PROJECT_ROOT / 'data'
-
-
-class EmailObject:
-    """Email Object is the core information from the announcement.csv"""
-
-    def __init__(self, **kwargs):
-
-        for key, value in kwargs.items():
-
-            if key == 'system':
-                system_list = value.split(';')
-                self._system = system_list
-
-            if key == 'services':
-                self._services = value
-
-            if key == 'starttime':
-                if not value:
-                    self._start_time = datetime.min
-                else:
-                    self._start_time = helpers.cd_to_datetime(value)
-
-            if key == 'endtime':
-                if not value:
-                    self._end_time = datetime.min
-                else:
-                    self._end_time = helpers.cd_to_datetime(value)
-
-            if key == 'downtime':
-                if value == 'Y':
-                    self._downtime = 'YES'
-                else:
-                    self._downtime = 'NO'
-
-            if key == 'approval':
-                if value == 'Y':
-                    self._approval = 'YES'
-                else:
-                    self._approval = 'NO'
-
-            if key == 'receivers':
-                if not value:
-                    self._receivers = None
-                else:
-                    mylist: list = value.split(';')
-                    self._receivers = mylist
-
-            if key == 'cc':
-                if value:
-                    mylist = value.split(';')
-                    self._cc = mylist
-                else:
-                    self._cc = None
-
-            if key == 'task':
-                self._task = value
-
-            if key == 'contact':
-                self._contact = value
-
-            if key == 'contact_no':
-                self._contact_no = value
-
-    @property
-    def receivers(self) -> list[str]:
-        return self._receivers
-
-    @property
-    def cc(self) -> list[str]:
-        return self._cc
-
-    @property
-    def system(self):
-        return self._system
-
-    @property
-    def services(self) -> str:
-        out = '<h3>Affected Services:</h3>'
-        items = self._services.split(";")
-        for item in items:
-            out += item + "<br>"
-        return out
-
-    def __str__(self):
-        return (f" Affected Systems: {list_to_string(self._system)} - "
-                f"Start Time: {helpers.datetime_to_str(self._start_time)}, "
-                f"Approval required: {str(self._approval)}, "
-                f"Downtime: {self._downtime}")
-
-    @property
-    def start_time(self):
-        return self._start_time
-
-    @property
-    def end_time(self):
-        return self._end_time
-
-
-def load_csv(file):
-    """Returns a list of EmailObject Objects"""
-
-    infile = pathlib.Path(file)
-    out = list()
-
-    with open(infile, 'r') as _io:
-        data = csv.DictReader(_io)
-        for row in data:
-            o_email = EmailObject(system=row['system'],
-                                  services=row['services'],
-                                  starttime=row['starttime'],
-                                  endtime=row['endtime'],
-                                  downtime=row['downtime'],
-                                  approval=row['approval'],
-                                  receivers=row['receiver'],
-                                  cc=row['cc'],
-                                  task=row['task'],
-                                  contact=row['contact'],
-                                  contact_no=row['contact_no']
-                                  )
-            out.append(o_email)
-    return out
-
-
-def list_to_string(i_list):
-    return ' '.join([str(elem) for elem in i_list])
-
 
 def send_mail(obj: EmailObject, subject="Announcement") -> None:
     sender_email = "notify@dbconcepts.at"
     message = MIMEMultipart("alternative")
 
-    message["From"] = "notify@dbconcepts.at"
+    message["From"] = input("Please put your email: ")
     message["To"] = ', '.join(obj.receivers)
 
     if obj.cc is None:
@@ -155,7 +28,7 @@ def send_mail(obj: EmailObject, subject="Announcement") -> None:
     env = Environment(loader=file_loader)
 
     if subject == "Maintenance":
-        template = env.get_template("maintenance.html")
+        template = env.get_template("maintenance.jinja")
         text = template.render(systems=obj.system,
                                  downtime=obj._downtime,
                                  starttime=obj.start_time,
@@ -166,11 +39,11 @@ def send_mail(obj: EmailObject, subject="Announcement") -> None:
                                  services=obj.services)
 
         message["Subject"] = f"Announcement: {subject} started on " \
-                             f"{list_to_string(obj.system)} " \
+                             f"{EmailObject.list_to_string(obj.system)} " \
                              f"Downtime: {obj._downtime} "
 
     elif subject == "Announcement":
-        template = env.get_template("announcement.html")
+        template = env.get_template("announcement.jinja")
         text = template.render(systems=obj.system,
                                  downtime=obj._downtime,
                                  starttime=obj.start_time,
@@ -182,13 +55,13 @@ def send_mail(obj: EmailObject, subject="Announcement") -> None:
 
         message["Subject"] = f"{subject}: " \
                              f"Upcoming maintenance on systems: " \
-                             f"{list_to_string(obj.system)}, " \
+                             f"{EmailObject.list_to_string(obj.system)}, " \
                              f"Approval required: {obj._approval}, " \
                              f"Downtime: {obj._downtime}, " \
                              f"Start Time: {obj.start_time} "
 
     elif subject == "Completed":
-        template = env.get_template("completed.html")
+        template = env.get_template("completed.jinja")
         text = template.render(systems=obj.system,
                                  downtime=obj._downtime,
                                  starttime=obj.start_time,
@@ -201,10 +74,10 @@ def send_mail(obj: EmailObject, subject="Announcement") -> None:
         message["Subject"] = f"Announcement: Maintenance " \
                              f"{subject.lower()} on " \
                              f"systems: " \
-                             f"{list_to_string(obj.system)} "
+                             f"{EmailObject.list_to_string(obj.system)} "
 
     elif subject == "Problem":
-        template = env.get_template("problem.html")
+        template = env.get_template("problem.jinja")
         text = template.render(systems=obj.system,
                                  downtime=obj._downtime,
                                  starttime=obj.start_time,
@@ -216,10 +89,10 @@ def send_mail(obj: EmailObject, subject="Announcement") -> None:
 
         message["Subject"] = f"Announcement: {subject} during " \
                              f"maintenance on systems: " \
-                         f"{list_to_string(obj.system)} occured!"
+                         f"{EmailObject.list_to_string(obj.system)} occured!"
 
     elif subject == "Reminder":
-        template = env.get_template("announcement.html")
+        template = env.get_template("announcement.jinja")
         text = template.render(systems=obj.system,
                                  downtime=obj._downtime,
                                  starttime=obj.start_time,
@@ -231,7 +104,7 @@ def send_mail(obj: EmailObject, subject="Announcement") -> None:
 
         message["Subject"] = f"{subject}: " \
                              f"Upcoming maintenance on systems: " \
-                             f"{list_to_string(obj.system)}, " \
+                             f"{EmailObject.list_to_string(obj.system)}, " \
                              f"Approval required: {obj._approval}, " \
                              f"Downtime: {obj._downtime}, " \
                              f"Start Time: {obj.start_time}, " \
@@ -240,10 +113,11 @@ def send_mail(obj: EmailObject, subject="Announcement") -> None:
     part1 = MIMEText(text, "html")
     message.attach(part1)
 
-    with smtplib.SMTP("localhost") as server:
+    with smtplib.SMTP(host="localhost",port=25, timeout=5) as server:
         try:
             server.send_message(message, from_addr=sender_email,
                                 to_addrs=recipients)
+            print(f"Message type: {subject} for {obj.system} was sent")
         except smtplib.SMTPException as err:
             print("Error during send message happened")
 
@@ -262,7 +136,7 @@ def f_argparser() -> object:
 
 def main():
     args = f_argparser()
-    data = load_csv(file=args.file)
+    data = EmailObject.load_csv(file=args.file)
 
     today = datetime.today().replace(hour=0,
                                      minute=0,
@@ -275,12 +149,9 @@ def main():
                                         microsecond=0)
 
     for email in data:
-        if today < email.start_time < midnight:
-            if args.system == list_to_string(email.system):
-                send_mail(email, args.type)
-                print("mail sent")
-            else:
-                send_mail(email)
+        listToStr = ' '.join(map(str, email.system))
+        if listToStr == args.system:
+            send_mail(email, args.type)
 
 
 if __name__ == "__main__":
